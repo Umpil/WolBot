87e13574cff37a11c07e5992d828a5f40e6fd813
Umpil <pavel.shcherban.0023@gmail.com>
2025-06-23 11:33:56 +0300

    Changes to be committed: 	modified:   .gitignore 	new file:   UserFiles/397555842/Mo_wl/output.txt 	new file:   UserFiles/397555842/Mo_wl/test.txt 	new file:   UserFiles/to_show/Mo_wl/Mo_wl.wl 	new file:   UserFiles/to_show/Mo_wl/Mo_wl_parsed.wl 	modified:   bot_core.py

--- bot_core.py
+++ bot_core.py
@@ -31,6 +31,64 @@ async def start_polling():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, relax=0.1, reset_webhook=True)
 
+async def run_wolphramscript(chat_id: int, file_path: str, namimg: str):
+    work_folder = "\\".join(file_path.split("\\")[0: -1])
+    except_files = [os.path.join(work_folder, namimg + ".wl"), file_path]
+    out_file_name = os.path.join(work_folder, "output.txt")
+    
+    with open(out_file_name, "w") as f:
+        f.write("dddd")
+
+    with open(os.path.join(work_folder, "test.txt"), "w") as f:
+        f.write("test")
+
+    # running_process = await asyncio.create_subprocess_exec(
+    #     "wolphramscript",
+    #     "-f",
+    #     file_path,
+    #     stdout=asyncio.subprocess.PIPE,
+    #     stderr=asyncio.subprocess.PIPE
+    # )
+    # stdout, stderr = await running_process.communicate()
+    
+    # with open(out_file_name, "w") as out:
+    #     if stdout:
+    #         out.write(str(stdout))
+    #     if stderr:
+    #         out.write(str(stderr))
+
+    files_after_run = [os.path.join(work_folder, x) for x in os.listdir(work_folder)]
+
+    # WOrk only with decimals
+    # files_to_send = set(files_before_run).symmetric_difference(files_after_run)
+    # print(files_to_send)
+    # for file_name in set(files_before_run) ^ set(files_after_run):
+    #     if file_name.endswith(["*jpg", ".png", "*tiff"]):
+            
+    #     else:
+    #         pass
+
+    files_to_send = []
+    # types.InputMediaPhoto() # for photo
+    # types.InputMediaDocument() # for documets
+    for file_name in files_after_run:
+        if file_name not in except_files:
+            if file_name.endswith(("*jpg", ".png", "*tiff")):
+                files_to_send.append(types.InputMediaPhoto(type="photo",
+                                                            media=types.FSInputFile(file_name)
+                                                            )
+                                    )
+            else:
+                files_to_send.append(types.InputMediaDocument(type="document", 
+                                                              media=types.FSInputFile(file_name)
+                                                              )
+                                    )
+    if files_to_send:
+        await bot.send_media_group(chat_id, list(files_to_send))
+    else:
+        await bot.send_message(chat_id, "Отсутсвует вывод программы")
+
+
 @dp.message(Command("start"))
 async def handle_start(message: types.Message):
     print(message.from_user.id)
@@ -58,6 +116,7 @@ async def handle_document(message: types.Message, state: FSMContext):
         parsed_filename = parse_file(destination)
         await state.set_state(ProcessingStates.WAITING_EXECUTION)
         await message.answer("Файл принят в обработку")
+        await run_wolphramscript(message.from_user.id, parsed_filename, now_name)
         
 
 asyncio.run(start_polling())
