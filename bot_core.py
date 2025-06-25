8df32d3ac45f6aa96e7268b9bf5e8808a39c933a
Umpil <pavel.shcherban.0023@gmail.com>
2025-06-25 21:33:42 +0300

    Changes to be committed: 	modified:   bot_core.py

--- bot_core.py
+++ bot_core.py
@@ -8,7 +8,7 @@ from aiogram.filters.state import State, StatesGroup
 from aiogram.fsm.context import FSMContext
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
-from aiogram.types import Document
+from aiogram.types import Document, ReplyKeyboardMarkup, KeyboardButton
 from collections import defaultdict
 
 ## mb redis???
@@ -28,7 +28,7 @@ async def start_polling():
     await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot, relax=0.1, reset_webhook=True)
 
-async def run_wolphramscript(chat_id: int, file_path: str, namimg: str):
+async def run_wolphramscript(chat_id: int, file_path: str, namimg: str, state: FSMContext):
     path = Path(file_path)
     work_folder = path.parent
     except_files = [os.path.join(work_folder, namimg + ".wl"), file_path]
@@ -86,6 +86,8 @@ async def run_wolphramscript(chat_id: int, file_path: str, namimg: str):
         await bot.send_media_group(chat_id, list(files_to_send))
     else:
         await bot.send_message(chat_id, "Отсутсвует вывод программы")
+    
+    await state.set_state(ProcessingStates.NOTHING)
 
 
 @dp.message(Command("start"))
@@ -95,63 +97,103 @@ async def handle_start(message: types.Message):
         await message.answer("Файл .wl")
 
 @dp.message(Command("cancel"))
-async def cancel_running(message: types.Message):
+async def cancel_running(message: types.Message, state: FSMContext):
     print("ADD CANCEL")
 
 @dp.message(F.media_group_id, F.document)
 async def handle_media_group(message: types.Message, state: FSMContext):
-    media_group_id = message.media_group_id
-    media_groups[media_group_id].append(message.document)
-    
-    await asyncio.sleep(1)  # Костыль
-    
-    if media_group_id in media_groups:
-        documents: list[Document]  = media_groups.pop(media_group_id)
-        check_list = [document.file_name.endswith(".wl") for document in documents]
-        how_many_wl_files = sum(check_list)
-        if not how_many_wl_files:
-            await message.answer("No wl")
-            return
-        elif how_many_wl_files == 1:
-            try:
-                wl_index = check_list.index(True)
-                wl_document = documents.pop(wl_index)
-                wl_file = await bot.get_file(wl_document.file_id)
-                wl_file_path = wl_file.file_path
-                user_folder = hash_user_id(message.from_user.id)
-                now_name = Path(wl_document.file_name).stem
-                now_folder = os.path.join(media_folder, user_folder, now_name)
-                if not os.path.exists(now_folder):
-                    os.makedirs(now_folder, exist_ok=True)
-                destination = os.path.join(now_folder, wl_document.file_name)
-                await bot.download_file(file_path=wl_file_path, destination=destination)
-                parsed_filename = parse_file(destination)
-                for document in documents:
-                    # File size ???
-                    file = await bot.get_file(document.file_id)
-                    file_path = file.file_path
-                    destination = os.path.join(now_folder, document.file_name)
-                    await bot.download_file(file_path=file_path, destination=destination)
-                await state.set_state(ProcessingStates.RUNNING)
-                await message.answer("Файлы приняты в обработку")
-                return await run_wolphramscript(message.from_user.id, parsed_filename, now_name)
-            except ValueError:
-                return await message.answer("Internal error")
-        else:
-            wl_files = [document.file_name for document, flag in zip(documents, check_list) if flag]
-            await state.update_data(documents=documents, wl_files=wl_files)
-    
-            await state.set_state(ProcessingStates.CLARIFY)
+    if message.from_user.id in ALLOWED_USERS:
+        if await state.get_state() == ProcessingStates.RUNNING:
+            return await message.answer("отмена /cancel")
+        media_group_id = message.media_group_id
+        media_groups[media_group_id].append(message.document)
+        
+        await asyncio.sleep(1)  # Костыль
+        
+        if media_group_id in media_groups:
+            documents: list[Document]  = media_groups.pop(media_group_id)
+            check_list = [document.file_name.endswith(".wl") for document in documents]
+            how_many_wl_files = sum(check_list)
+            if not how_many_wl_files:
+                await message.answer("No wl")
+                return
+            elif how_many_wl_files == 1:
+                try:
+                    wl_index = check_list.index(True)
+                    wl_document = documents.pop(wl_index)
+                    wl_file = await bot.get_file(wl_document.file_id)
+                    wl_file_path = wl_file.file_path
+                    user_folder = hash_user_id(message.from_user.id)
+                    now_name = Path(wl_document.file_name).stem
+                    now_folder = os.path.join(media_folder, user_folder, now_name)
+                    if not os.path.exists(now_folder):
+                        os.makedirs(now_folder, exist_ok=True)
+                    destination = os.path.join(now_folder, wl_document.file_name)
+                    await bot.download_file(file_path=wl_file_path, destination=destination)
+                    parsed_filename = parse_file(destination)
+                    for document in documents:
+                        # File size ???
+                        file = await bot.get_file(document.file_id)
+                        file_path = file.file_path
+                        destination = os.path.join(now_folder, document.file_name)
+                        await bot.download_file(file_path=file_path, destination=destination)
+                    await state.set_state(ProcessingStates.RUNNING)
+                    await message.answer("Файлы приняты в обработку")
+                    return await run_wolphramscript(message.from_user.id, parsed_filename, now_name)
+                except ValueError:
+                    return await message.answer("Internal error")
+            else:
+                wl_files = [document.file_name for document, flag in zip(documents, check_list) if flag]
+                await state.update_data(documents=documents, wl_files=wl_files)
+                keyboard = create_keyboard(wl_files)
+                await message.answer(text="Выберите файл для запуска", reply_markup=keyboard)
+                await state.set_state(ProcessingStates.CLARIFY)
 
 @dp.message(ProcessingStates.CLARIFY)
-def clarify_which_file_to_run(message: types.Message, state: FSMContext):
-    pass
+async def clarify_which_file_to_run(message: types.Message, state: FSMContext):
+    if message.from_user.id in ALLOWED_USERS:
+        message_text = message.text
+        data = await state.get_data()
+        wl_files: list[str] = data["wl_files"]
+        documents: list[Document] = data["documents"]
+        if message_text == "Отмена":
+            await state.set_state(ProcessingStates.NOTHING.state)
+            return await message.answer("Отменено", reply_markup=types.ReplyKeyboardRemove())
+        elif message_text not in wl_files:
+            return await message.answer("Выберите название файла")
+        wl_document = [document for document in documents if document.file_name == message_text][0]
+        documents.remove(wl_document)
+        wl_file = await bot.get_file(wl_document.file_id)
+        wl_file_path = wl_file.file_path
+        user_folder = hash_user_id(message.from_user.id)
+        now_name = Path(wl_document.file_name).stem
+        now_folder = os.path.join(media_folder, user_folder, now_name)
+        if not os.path.exists(now_folder):
+            os.makedirs(now_folder, exist_ok=True)
+        destination = os.path.join(now_folder, wl_document.file_name)
+        await bot.download_file(file_path=wl_file_path, destination=destination)
+        parsed_filename = parse_file(destination)
+        for document in documents:
+            # File size ???
+            wl_file = await bot.get_file(document.file_id)
+            wl_file_path = wl_file.file_path
+            destination = os.path.join(now_folder, document.file_name)
+            await bot.download_file(file_path=wl_file_path, destination=destination)
+            if document.file_name.endswith(".wl"):
+                parse_file(destination)
+        
+        await state.set_state(ProcessingStates.RUNNING)
+        await message.answer("Файлы приняты в обработку")
+        return await run_wolphramscript(message.from_user.id, parsed_filename, now_name, state)
+
 
 @dp.message(F.document)
 async def handle_document(message: types.Message, state: FSMContext):
     if message.from_user.id in ALLOWED_USERS:
         if await state.get_state() == ProcessingStates.RUNNING.state:
             return await message.answer("отмена /cancel")
+        if await state.get_state() == ProcessingStates.CLARIFY.state:
+            await state.set_state(ProcessingStates.NOTHING.state)
         document = message.document
         await message.answer(f"Файл {document.file_name}")
         file_name = document.file_name
@@ -170,7 +212,25 @@ async def handle_document(message: types.Message, state: FSMContext):
         parsed_filename = parse_file(destination)
         await state.set_state(ProcessingStates.WAITING_EXECUTION)
         await message.answer("Файл принят в обработку")
-        return await run_wolphramscript(message.from_user.id, parsed_filename, now_name)
+        return await run_wolphramscript(message.from_user.id, parsed_filename, now_name, state)
         
+
+def create_keyboard(options: list[str], cancel_btn: str = "Отмена") -> ReplyKeyboardMarkup:
+    keyboard = []
+    row = []
+    callback_data = 0
+    for option in options:
+        row.append(KeyboardButton(text=option, callback_data=str(callback_data)))
+        if len(row) == 2:
+            keyboard.append(row)
+            row = []
+        callback_data += 1
+    
+    if row:
+        keyboard.append(row)
+
+    keyboard.append([KeyboardButton(text=cancel_btn, callback_data="-1")])
+    
+    return ReplyKeyboardMarkup(keyboard=keyboard, resize_keyboard=True)
 
 asyncio.run(start_polling())
